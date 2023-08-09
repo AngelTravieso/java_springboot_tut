18d5e52c0fc4de4c5b42438c38575e257d849f04
AngelTravieso <angeltravieso@gmail.com>
2023-08-09 09:36:35 -0400

    Insertar nuevo estudiante

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -23,6 +25,15 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = this.studentRepository.findStudentByEmail(student.getEmail());
+
+        // Si el email existe
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("Email taken");
+        }
+
+        // System.out.println(student);
+
+        this.studentRepository.save(student);
     }
 }
